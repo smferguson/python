bcc326b628587deec65398c9291ab9e1bac488f8
Scott <smferguson@users.noreply.github.com>
2016-09-22 16:17:13 -0400

    bar

--- text_utils.py
+++ text_utils.py
@@ -7,4 +7,4 @@ def CamelToSnake(name):
   # TODO: if heavily used compile the regexs
   s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
   s2 = re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
-  return s2.replace(' ', '').replace('-','_')
+  return s2.replace(' ', '').replace('-','')
